bd0b8ceee53c1c88c755ecaa0457adfcace44b66
BillyOfficial <arthurferreira183@hotmail.com>
2023-09-14 15:08:07 -0300

    Initial commit

---
+++ D3.py
@@ -0,0 +1,5 @@
+print('================== Desafio 3 ======================')
+n1 = int(input('Primeiro valor: '))
+n2 = int(input('Segundo valor: '))
+r = n1 + n2
+print(f'A soma entre {n1} e {n2} é {r}!')
