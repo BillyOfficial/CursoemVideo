76d9ce359a9e54840c0700da2286b9ef2c824ef8
BillyOfficial <arthurferreira183@hotmail.com>
2023-09-16 16:31:36 -0300

    Desafio 06 concluído com êxito!
    Achei que iria ser complicado, mas até que foi fácil...

--- D006.py
+++ D006.py
@@ -1,2 +1,4 @@
 # Crie um algoritmo que leia um número e mostre o seu dobro, triplo a raiz quadrada.
 print('================== Desafio 6 ======================')
+n0 = int(input('Digite um número: '))
+print(f'O dobro de {n0} é {n0*2}, o triplo é {n0*3} e a raiz quadrada é {n0**(1/2)}.')
