e01bef432f348a19a8cdd8446effe0254fdc7260
BillyOfficial <arthurferreira183@hotmail.com>
2023-09-16 18:00:39 -0300

    Desafio 08 concluído com êxito!

--- D008.py
+++ D008.py
@@ -1,2 +1,4 @@
-# Escreva um programa que leia um valor em metros a o axiba convertido em centímetros e milimetros.
+# Escreva um programa que leia um valor em metros a o exiba convertido em centímetros e milímetros.
 print('================== Desafio 8 ======================')
+p1 = int(input('Digite a quantidade de metros que deseja calcular: '))
+print(f'O metro informado equivale a {p1*100} centímetros e {p1*1000} milímetros.')
