f32ab4c083b94a43f851ca8f9f8944f01e276b6a
BillyOfficial <arthurferreira183@hotmail.com>
2023-09-16 18:13:29 -0300

    Desafio 09 concluído com êxito!

--- D009.py
+++ D009.py
@@ -1,2 +1,14 @@
-# Faça um programa que leia um número Inteiro qualquer e mostre na tela a sua tabuada.
+# Faça um programa que leia qualquer número inteiro e mostre na tela a sua tabuada.
 print('================== Desafio 9 ======================')
+p1 = int(input(' Digite um número inteiro: '))
+print(f'========== Tabuada de {p1} ==========\n'
+      f'{p1}x1={p1*1}\n'
+      f'{p1}x2={p1*2}\n'
+      f'{p1}x3={p1*3}\n'
+      f'{p1}x4={p1*4}\n'
+      f'{p1}x5={p1*5}\n'
+      f'{p1}x6={p1*6}\n'
+      f'{p1}x7={p1*7}\n'
+      f'{p1}x8={p1*8}\n'
+      f'{p1}x9={p1*9}\n'
+      f'{p1}x10={p1*10}\n')
