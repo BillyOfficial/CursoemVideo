90dad6855ef2dcf5fe2e0b50a546daca2624732b
BillyOfficial <arthurferreira183@hotmail.com>
2023-09-17 09:32:55 -0300

    Alterando o "int" por "float" e inserindo uma nova cotação do dolar de hj (dom.17/09/23).

--- D010.py
+++ D010.py
@@ -1,6 +1,6 @@
 # Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos Dólares ela pode comprar.
 # Considere US$1.00 = R$3.27
 print('================== Desafio 10 ======================')
-p1 = int(input('Quantos reais você quer investir? '))
-dolar = 3.27
-print(f'Você pode comprar {p1//dolar} dólares seguindo a cotação de US$1.00 = R${dolar}')
+p1 = float(input('Quantos reais você quer investir? '))
+dolar = 4.87
+print(f'Você pode comprar {p1/dolar:.2f} dólares seguindo a cotação de US$1.00 = R${dolar}')
