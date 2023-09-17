60d67ef5616eecc0d93b50f9e0214c28c71140ad
BillyOfficial <arthurferreira183@hotmail.com>
2023-09-16 22:39:24 -0300

    Desafio 12 concluído com êxito!

--- D012.py
+++ D012.py
@@ -1,2 +1,4 @@
 # Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
 print('================== Desafio 12 ======================')
+p = int(input('Digite o preço do produto: '))
+print(f'Com o desconto de 5% o produto fica por: {p-(p/100*5)}')
