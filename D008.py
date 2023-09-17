f57412015db9ef2b1c94a3d87c37deb6efa79e25
BillyOfficial <arthurferreira183@hotmail.com>
2023-09-17 09:22:03 -0300

    Aceitando o novo desafio 08, inserindo no print os quilômetros, hectômetro, decâmetro, metro e decímetro.

--- D008.py
+++ D008.py
@@ -1,4 +1,11 @@
 # Escreva um programa que leia um valor em metros a o exiba convertido em centímetros e milímetros.
 print('================== Desafio 8 ======================')
 p1 = int(input('Digite a quantidade de metros que deseja calcular: '))
-print(f'O metro informado equivale a {p1*100} centímetros e {p1*1000} milímetros.')
+print(f'O metro informado equivale a:'
+      f'\n {p1/1000} quilômetros.'
+      f'\n {p1/100} hectômetro.'
+      f'\n {p1/10} decâmetro.'
+      f'\n {p1} metros.'
+      f'\n {p1*10} decímetros.'
+      f'\n {p1*100} centímetros.'
+      f'\n {p1*1000} milímetros. ')
