9385c771427816ca6a8a4c8861e336771c4fd6a5
BillyOfficial <arthurferreira183@hotmail.com>
2023-09-16 23:10:25 -0300

    Desafio 11 concluído com êxito!

--- D011.py
+++ D011.py
@@ -2,3 +2,7 @@
 # calcule a sua área e a quantidade de tinta necessária para pintá-la,
 # sabendo que cada litro de tinta pinta uma área de 2m².
 print('================== Desafio 11 ======================')
+lar = float(input('Digite a largura: '))
+alt = float(input('Digite a altura: '))
+print(f'Você tem {lar*alt} metros quadrados para pintar!')
+print(f'Você gastaria {lar*alt/2} litros.')
