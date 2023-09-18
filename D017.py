358722a49151321e1acb18c2996ab33d5780d336
BillyOfficial <arthurferreira183@hotmail.com>
2023-09-18 18:13:29 -0300

    Criando o Desafio 017 e concluído com êxito.

--- D017.py
+++ D017.py
@@ -1,1 +1,5 @@
+from math import sqrt
 print('================== Desafio 17 ======================')
+catopo = float(input('Digite o comprimento do cateto oposto:'))
+catadj = float(input('Digite o comprimento do cateto adjacente: '))
+print(f'O comprimento da hipotenusa é {sqrt(catopo**2+catadj**2):.2f}')
