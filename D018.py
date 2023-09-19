85ef38b9a16edb5b6c8615666b41eee0631d2b62
BillyOfficial <arthurferreira183@hotmail.com>
2023-09-18 21:35:31 -0300

    Desafio 18 concluído. Deu trabalho, mas conclui com êxito :D

--- D018.py
+++ D018.py
@@ -1,1 +1,12 @@
+import math
 print('================== Desafio 18 ======================')
+angulo = float(input('Digite o ângulo desejado: '))
+
+radiano = angulo*3.14/180
+seno = math.sin(radiano)
+cosseno = math.cos(radiano)
+tangente = math.tan(radiano)
+
+print(f'Seno:.......{seno:.2f}\n'
+      f'Cosseno:....{cosseno:.2f}\n'
+      f'Tangente:...{tangente:.2f}')
