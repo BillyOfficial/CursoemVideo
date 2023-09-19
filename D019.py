e521472d98fd6cf781cee4409bb9f28ac7f0d9c5
BillyOfficial <arthurferreira183@hotmail.com>
2023-09-19 19:48:30 -0300

    Desafio 019 concluído com êxito!
    Achei fácil, tem tudo mastigado no documento do pyhton.

--- D019.py
+++ D019.py
@@ -1,1 +1,7 @@
+from random import choice
 print('================== Desafio 19 ======================')
+prialuno = str(input('Digite o nome do primeiro aluno: '))
+segaluno = str(input('Digite o nome do segundo aluno: '))
+teraluno = str(input('Digite o nome do terceiro aluno: '))
+quaaluno = str(input('Digite o nome do quarto aluno: '))
+print(f'O aluno escolhido foi {choice([prialuno, segaluno, teraluno, quaaluno])}')
