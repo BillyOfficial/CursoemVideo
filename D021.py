678347f00cd798bd22c26a0e5e514db7b8ec4b1b
BillyOfficial <arthurferreira183@hotmail.com>
2023-09-19 23:16:30 -0300

    Concluído o desafio 21 com êxito
    Essa eu tive que pesquisar na janela PyPI, quase dei um google kkkkk

--- D021.py
+++ D021.py
@@ -1,1 +1,4 @@
+from audioplayer import AudioPlayer
 print('================== Desafio 21 ======================')
+print('Você está escutando Roddy Ricch - The Box!')
+AudioPlayer("desafio21/the_box.mp3").play(block=True)
