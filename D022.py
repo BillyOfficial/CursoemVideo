6af89ef523e1c6470bc2457b6c7b40c5cb3f4ea2
BillyOfficial <arthurferreira183@hotmail.com>
2023-09-20 14:31:58 -0300

    Criação do exercício

---
+++ D022.py
@@ -0,0 +1,7 @@
+# Desafio 22
+# Crie um programa que leia o nome completo de uma pessoa e mostre:
+# O nome com todas as letras maiúsculas.
+# O nome com todas as letras minúsculas.
+# Quantas letras no total sem considerar espaços.
+# Quantas letras possui no primeiro nome.
+print('================== Desafio 22 ======================')
