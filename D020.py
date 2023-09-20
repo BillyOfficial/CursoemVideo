1b5ad794bea0eeff6809ff8b65c633a125de5380
BillyOfficial <arthurferreira183@hotmail.com>
2023-09-19 23:17:10 -0300

    Desafio 020 concluído com êxito!
    Nada a declarar, fácil tbm

--- D020.py
+++ D020.py
@@ -1,1 +1,7 @@
+from random import sample
 print('================== Desafio 20 ======================')
+aluno1 = str(input('Digite o nome do aluno: '))
+aluno2 = str(input('Digite o nome do aluno: '))
+aluno3 = str(input('Digite o nome do aluno: '))
+aluno4 = str(input('Digite o nome do aluno: '))
+print(f'A ordem é {sample([aluno1, aluno2, aluno3, aluno4], k=4)}.')
