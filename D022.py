fd0292975af0d0fa390cc64f4d681b1d9517763b
BillyOfficial <arthurferreira183@hotmail.com>
2023-09-21 15:54:34 -0300

    Dando um upgrade dps da aula do guanabara.

--- D022.py
+++ D022.py
@@ -5,13 +5,9 @@
 # O nome com todas as letras minúsculas.
 # Quantas letras no total sem considerar espaços.
 # Quantas letras possui no primeiro nome.
-
 print('================== Desafio 22 ======================')
-name = str(input('Digite o seu nome completo: '))
-nspace = name.strip()
-print(nspace.upper())  # Letra maior
-print(nspace.lower())  # Letra menor
-print(len(nspace)-nspace.count(' '))  # Contar as letras - space
-print(len(nspace.split()[0]))  # Quantas letras no primeiro name
-
-# Arthur da Conceição Ferreira
+name = str(input('Digite o seu nome completo: ')).strip()
+print(name.upper())  # Letra maior
+print(name.lower())  # Letra menor
+print(len(name)-name.count(' '))  # Contar as letras menos o space
+print(len(name.split()[0]))  # Quantas letras no primeiro name
