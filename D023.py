0b9db30e664dfdf998a7b409ac8b2a4360f1f019
BillyOfficial <arthurferreira183@hotmail.com>
2023-09-21 15:52:41 -0300

    Desafio 023 concluído com êxito!

--- D023.py
+++ D023.py
@@ -2,3 +2,8 @@
 # E mostre na tela cada um dos dígitos separados.
 
 print('================== Desafio 23 ======================')
+number = str(input('Digite um número de 0 a 9999: '))
+print(f'Primeiro número: {number[0]}\n'
+      f'Segundo número:  {number[1]}\n'
+      f'Terceiro número: {number[2]}\n'
+      f'Quarto número:   {number[3]}')
