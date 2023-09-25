8d53f7f77c564c7bf17c9eee6b4db683e17d9544
BillyOfficial <arthurferreira183@hotmail.com>
2023-09-25 20:37:12 -0300

    Atualizando...

--- D026.py
+++ D026.py
@@ -1,5 +1,6 @@
-# Faça um programa que leia uma frase pelo teclado.
-# E mostre quantas vezes aparece a letra "A", em que posição ela aparece a primeira vez.
+# Faça um programa que leia uma frase.
+# E mostre quantas vezes aparece a letra "A"
+# Em que posição ela aparece a primeira vez.
 # Também em que posição ela aparece a última vez.
 
 print('================== Desafio 26 ======================')
