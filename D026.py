b99a06c3135126deb42074855061ab78f2e9a1ed
BillyOfficial <arthurferreira183@hotmail.com>
2023-09-25 23:23:57 -0300

    Terminei o exercicío, mas acho que ainda da para melhorar.

--- D026.py
+++ D026.py
@@ -4,3 +4,10 @@
 # Também em que posição ela aparece a última vez.
 
 print('================== Desafio 26 ======================')
+frase = str(input('Digite uma frase: ')).strip().upper()
+print(f'Possui '
+      f'{frase.count("A") + frase.count("Á") + frase.count("Ã") + frase.count("Â") + frase.count("À")} '
+      f'letras "a" na frase.')
+print(f"A primeira letra fica na {frase.find('A') + 1}ª posição.")
+print(f"E a última letra fica na {frase.rfind('A') + 1}ª posição.")
+
