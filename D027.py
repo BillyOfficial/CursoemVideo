765c5d7fe11b143dae72bdaf526273f36385a98f
BillyOfficial <arthurferreira183@hotmail.com>
2023-09-27 11:57:29 -0300

    Desafio concluído com sucesso!
    Izi d+

--- D027.py
+++ D027.py
@@ -2,3 +2,5 @@
 # Mostrando em seguida o primeiro e o último nome separadamente.
 
 print('================== Desafio 27 ======================')
+nome = str(input('Digite seu nome completo: ')).strip().title()
+print(f'Seu primeiro nome é {nome[0:nome.find(" ")]} e sobrenome {nome[nome.rfind(" ") +1 :50]}.')
