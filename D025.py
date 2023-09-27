808828b7df6bf23c525c6260dbea986e197f5882
BillyOfficial <arthurferreira183@hotmail.com>
2023-09-27 11:35:02 -0300

    Incluí anotações

--- D025.py
+++ D025.py
@@ -3,7 +3,7 @@
 
 print('================== Desafio 25 ======================')
 nome = (str(input('Digite um nome completo: '))).strip().title()
-nome2 = nome.find('Silva') + 1
-nome3 = 'Silva' in nome
-print(f'Você tem o sobrenome Silva? {bool(nome2)}')
-print(f'Você tem o sobrenome Silva? {nome3}')
+nome2 = nome.find('Silva') + 1 # Método 1
+nome3 = 'Silva' in nome # Método 2
+print(f'Você tem o sobrenome Silva? {bool(nome2)}') # Método 1
+print(f'Você tem o sobrenome Silva? {nome3}') # Método 2
