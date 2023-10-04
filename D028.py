79f9ae6735ddc25051a0540a30a1a3a237c35329
BillyOfficial <arthurferreira183@hotmail.com>
2023-10-03 22:16:44 -0300

    Desafio 028 concluído com êxito!
    Top d+

--- D028.py
+++ D028.py
@@ -2,4 +2,17 @@
 # E peça para o usuário tentar descobrir qual foi o número escolhido pelo computador.
 # O programa deverá escrever na tela se o usuário venceu ou perdeu.
 
+from random import choice
 print('================== Desafio 28 ======================')
+print("Pensarei em um número de 0 a 5, tente adivinhar!")
+
+aleatorio = choice([1, 2, 3, 4, 5])
+tentativa = int(input("Digite um número de 0 a 5: "))
+
+if tentativa > 5:
+    print("Por favor, digite um número entre 0 e 5.")
+else:
+    if aleatorio == tentativa:
+        print("Parabéns você acertou!")
+    else:
+        print(f"Ops, você errou, eu pensei em {aleatorio}")
