882e48a412a2aa1c9f88f24ff131cc1dd523150f
BillyOfficial <arthurferreira183@hotmail.com>
2023-10-05 16:44:17 -0300

    Desafio 030 concluído com êxito!
    Desafio TOP!!!

--- Exercicios/D030.py
+++ Exercicios/D030.py
@@ -2,3 +2,9 @@
 # e mostre na tela se ele é PAR ou ÍMPAR.
 
 print('================== Desafio 30 ======================')
+print('Irei descobrir se o número é par ou ímpar!')
+numero = int(input("Digite um número: "))
+if numero % 2 == 0:
+    print("O número é par.")
+else:
+    print("O número é ímpar.")
