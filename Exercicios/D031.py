c4ee4ac5b6d6eb43110c65ee74bfe18a81e4ad48
BillyOfficial <arthurferreira183@hotmail.com>
2023-10-06 20:34:10 -0300

    Desafio 031 concluído com êxito!

--- Exercicios/D031.py
+++ Exercicios/D031.py
@@ -4,3 +4,12 @@
 # e R$0.45 por quilômetro para viagens mais longas.
 
 print('================== Desafio 31 ======================')
+print('Bem vindo ao programa de cálculo de Quilômetros!')
+km = float(input('Digite quantos km serão a viagem: '))
+menos200 = km * 0.50
+mais200 = km * 0.45
+
+if km < 200:
+    print(f'A passagem custa R${menos200:.2f}')
+else:
+    print(f'A passagem custa R${mais200:.2f}')
