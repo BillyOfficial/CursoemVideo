fbca82018774df06f30fb3e090240761313de6bc
BillyOfficial <arthurferreira183@hotmail.com>
2023-10-06 21:01:56 -0300

    Desafio 032 concluído com êxito!

--- Exercicios/D032.py
+++ Exercicios/D032.py
@@ -2,3 +2,10 @@
 # e determine se esse ano é bissexto.
 
 print('================== Desafio 32 ======================')
+
+ano = int(input("Digite o ano: "))
+
+if ano % 4 == 0:
+    print('É um ano bissexto!')
+else:
+    print('Não é um ano bissexto!')
