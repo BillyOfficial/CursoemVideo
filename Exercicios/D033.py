697665df8a7589de81405dc2a4515720acd59f14
BillyOfficial <arthurferreira183@hotmail.com>
2023-10-23 13:16:51 -0300

    Desafio 033 concluído com êxito!

--- Exercicios/D033.py
+++ Exercicios/D033.py
@@ -2,3 +2,13 @@
 # e mostre qual é o maior e qual é o menor.
 
 print('================== Desafio 33 ======================')
+
+numero1 = int(input('Digite o primeiro o número: '))
+numero2 = int(input('Digite o segundo o número: '))
+numero3 = int(input('Digite o terceiro o número: '))
+
+lista = [numero1, numero2, numero3]
+ordem = sorted(lista)
+
+print(f'O menor número é {ordem[0]}')
+print(f'O maior número é {ordem[2]}')
