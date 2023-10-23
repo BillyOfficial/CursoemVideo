4f739df05400cfe1624b80c14809be77a001dc66
BillyOfficial <arthurferreira183@hotmail.com>
2023-10-23 13:44:38 -0300

    Desafio 35 criado.

---
+++ Exercicios/D035.py
@@ -0,0 +1,4 @@
+# Desenvolva um programa que leia o comprimento de três retas
+# E diga ao usuário se elas podem ou não formar um triângulo.
+
+print('================== Desafio 35 ======================')
