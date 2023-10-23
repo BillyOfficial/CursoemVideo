b09570e967576bb79b912bad5495a142ffaf4a0b
BillyOfficial <arthurferreira183@hotmail.com>
2023-10-23 19:43:07 -0300

    Desafio 35 concluído com êxito!

--- Exercicios/D035.py
+++ Exercicios/D035.py
@@ -2,3 +2,12 @@
 # E diga ao usuário se elas podem ou não formar um triângulo.
 
 print('================== Desafio 35 ======================')
+
+seg1 = float(input('Digite o primeiro seguimento: '))
+seg2 = float(input('Digite o segundo seguimento: '))
+seg3 = float(input('Digite o terceiro seguimento: '))
+
+if seg1 + seg2 > seg3 and seg1 + seg3 > seg2 and seg2 + seg3 > seg1:
+    print('Com essas medidas, PODE fazer um triângulo!')
+else:
+    print('Com essas medidas, é IMPOSSÍVEL fazer um triângulo!')
